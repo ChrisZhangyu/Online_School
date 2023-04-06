67c5a64cd579b7acf900daeecf3521101f4ce8e9
chris <chris@123.com>
2023-04-06 21:21:57 +0800

    docker打包中间阶段

--- common/service_utils/src/main/java/com/chris/onlineSchool/utils/RedisUtils.java
+++ common/service_utils/src/main/java/com/chris/onlineSchool/utils/RedisUtils.java
@@ -1,4 +1,9 @@
 package com.chris.onlineSchool.utils;
 
+import java.util.Stack;
+
 public class RedisUtils {
+    public static void main(String[] args) {
+        Stack stack = new Stack<Integer>();
+    }
 }
--- service/service_user/src/main/java/com/chris/onlineSchool/user/service/impl/UserServiceImpl.java
+++ service/service_user/src/main/java/com/chris/onlineSchool/user/service/impl/UserServiceImpl.java
@@ -44,4 +44,67 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserInfo> implement
             return "";
         }
     }
+
+
+    public  static int strStr(String haystack, String needle) {
+        int length = haystack.length();
+        int [] next = new int [length];
+
+        getNext(next, needle);
+        for(int item : next){
+            System.out.print(item + " ");
+        }
+        for(int i = 0, j = 0; i < length; ){
+            if(haystack.charAt(i) == needle.charAt(j)){
+                j++;
+                i++;
+            }else if(j > 0){
+                j = next[j - 1];
+            }
+            if(j == needle.length()){
+                return i - j + 1;
+            }
+        }
+        return -1;
+    }
+
+//    public static int strStr(String haystack, String needle) {
+//        if (needle.length() == 0) return 0;
+//        int[] next = new int[needle.length()];
+//        getNext(next, needle);
+//
+//        int j = 0;
+//        for (int i = 0; i < haystack.length(); i++) {
+//            while (j > 0 && needle.charAt(j) != haystack.charAt(i))
+//                j = next[j - 1];
+//            if (needle.charAt(j) == haystack.charAt(i))
+//                j++;
+//            if (j == needle.length())
+//                return i - needle.length() + 1;
+//        }
+//        return -1;
+//
+//    }
+
+    private static void getNext(int[] next, String s) {
+        for(int i = 1, j = 0; i < s.length() - 1;i++){
+            if( s.charAt(i) == s.charAt(j)){
+                j++;
+                next[i] = j;
+
+            }else if(j > 0){
+                j = next[j - 1];
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "mississippi";
+        String needle = "issip";
+//        int [] next = new int[s.length()];
+        strStr(s, needle);
+    }
+
+
 }
